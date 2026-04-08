a618f6a8e58c2b51801c23172b9b05ebedca756d
MarioJGC <77269474mario@gmail.com>
2026-04-08 16:32:28 +0200

    rm: texto en el archivo

--- src/main/java/config/AppConfig.java
+++ src/main/java/config/AppConfig.java
@@ -7,11 +7,7 @@ package config;
 import jakarta.ws.rs.ApplicationPath;
 import jakarta.ws.rs.core.Application;
 
-/**
- * Configuración del servicio REST - define el base path como /api
- *
- * @author alumne
- */
+
 @ApplicationPath("/api")
 public class AppConfig extends Application {
 }
--- src/main/java/modelo/Video.java
+++ src/main/java/modelo/Video.java
@@ -4,10 +4,6 @@
  */
 package modelo;
 
-/**
- *
- * @author alumne
- */
 public class Video {
 
     private int id;
